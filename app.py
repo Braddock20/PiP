ad74d1b16134931330c805c701761dddbddce76a
Braddock20 <braddockikubano@gmail.com>
2026-01-05 17:09:32 +0300

    Update main block to specify host and port

--- app.py
+++ app.py
@@ -193,7 +193,8 @@ def index():
 
     return render_template('index.html')
 
-if __name__ == '__main__':
-    # Threaded=True helps prevent blocking on download
-    app.run(debug=True, threaded=True)
-          
+if __name__ == "__main__":
+    app.run(
+        host="0.0.0.0",
+        port=int(os.environ.get("PORT", 5000))
+    )
